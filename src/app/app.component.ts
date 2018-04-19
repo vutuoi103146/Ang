8dddedd6b4d7d3125d5d9b4b2ddad1cfcd4cef66
Le Anh Vu <phongsonpt@yahoo.com>
2018-04-19 15:13:42 +0700

    Lession 11

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: '  <h4>This is {{title}} </h4> <input type ="text" [value] ="image" /> <my-tutorial></my-tutorial> <button (click) = "OnClick(name.value)">OK</button>'
+  template: '  <h4>This is {{title}} </h4> <input type ="text" [value] ="image" /> <my-tutorial></my-tutorial>'
 })
 export class AppComponent {
   public title ="App Component"
   public image ="Input text to seach"
-  public OnClick()
-  {
-    this.image ="Running Click";
-    this.title ="Click Me"
-  }
 }
--- src/app/tutorial.component.ts
+++ src/app/tutorial.component.ts
@@ -1,9 +1,24 @@
 import { Component } from '@angular/core';
 @Component({
     selector: 'my-tutorial',
-    template:'<input type="text" [(ngModel)] ="fname" /> <input type="text" [(ngModel)] ="lname" /> <br> Full name: {{fname}} {{lname}} <br>'
+    template:   `
+                <h3 *ngIf="showLineIf"> This is Lesson 11</h3> 
+                <div [ngSwitch] ="color">
+                    <p *ngSwitchCase ="'red'">This is color red</p>
+                    <p *ngSwitchCase ="'blue'">This is color blue</p>
+                    <p *ngSwitchCase ="'yellow'">This is color yellow</p>
+                    <p *ngSwitchDefault>This is color yellow</p>
+                </div>
+                <ul>
+                    <li *ngFor = "let color of colors">{{color}}</li>
+                </ul>
+                `
 
 })
+
 export class TutorialComponent
 {
+    public showLineIf = false;
+    public color ="yellow";
+    public colors: string[] = ["red","blue","yellow"];
 }
