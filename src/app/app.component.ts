bbeafd8bec8014d07315d22356b5d7da7a8af08c
Le Anh Vu <phongsonpt@yahoo.com>
2018-04-19 16:03:23 +0700

    Compnent Interactions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: '  <h4>This is {{title}} </h4> <input type ="text" [value] ="image" /> <my-tutorial></my-tutorial>'
+  template: `
+          <h4>This is {{title}} </h4> <my-tutorial></my-tutorial>
+  `  
 })
 export class AppComponent {
   public title ="App Component"
-  public image ="Input text to seach"
 }
--- src/app/tutorial.component.ts
+++ src/app/tutorial.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 @Component({
     selector: 'my-tutorial',
-    template:'<h4>This is Tutorial Component</h4>'
+    template:`
+            <h4>Hello {{persion}}</h4>
+    `
 })
 export class TutorialComponent
 {
-
+    public persion = "Mr Anh Vũ";
 }
