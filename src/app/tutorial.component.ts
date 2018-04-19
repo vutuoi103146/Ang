8019539c2ab5052c6b25218c8d943153d3e8bdd8
Le Anh Vu <phongsonpt@yahoo.com>
2018-04-19 11:06:28 +0700

    Style

--- src/app/tutorial.component.ts
+++ src/app/tutorial.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 @Component({
     selector: 'my-tutorial',
-    template:'<h4>This is Tutorial Component</h4>'
+    template:'<h4>This is Tutorial Component</h4> <h3 [class.redColor] ="applyClass">Apply Class</h3> <h3 [style.color] = "colorblue" >Style Class</h3>',
+    styles:['.redColor{ color: Red;}']
 })
 export class TutorialComponent
 {
 
+    public applyClass = true;
+    public colorblue = "blue";
 }
