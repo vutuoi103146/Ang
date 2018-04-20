bb9f1b3b2ed0b0d24e38b39cb3bb88e02f510e6a
Le Anh Vu <phongsonpt@yahoo.com>
2018-04-20 09:00:53 +0700

    Truyen bien giua compo cha va con

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-          <h4>This is {{title}} </h4> <my-tutorial></my-tutorial>
+          <h4>This is {{title}} 
+          <input type="text" #txtName (keyup)="0" />
+          </h4> <my-tutorial [name] ="txtName.value"></my-tutorial>
   `  
 })
 export class AppComponent {
--- src/app/tutorial.component.ts
+++ src/app/tutorial.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 @Component({
     selector: 'my-tutorial',
-    template:`
+    template:`<hr>
             <h4>Hello {{persion}}</h4>
+            <p>Component Tutotial: {{name}}</p>
     `
 })
 export class TutorialComponent
 {
     public persion = "Mr Anh Vũ";
+    @Input() name:string ="";
 }
