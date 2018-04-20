c5399a06f21ca02135ef98e1beccef1e3e120b9b
Le Anh Vu <phongsonpt@yahoo.com>
2018-04-20 10:02:05 +0700

    Leson13

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,36 @@
-import { Component } from '@angular/core';
-
+import { Component, ViewChild } from '@angular/core';
+import {TutorialComponent} from './tutorial.component'
 @Component({
   selector: 'app-root',
   template: `
           <h4>This is {{title}} 
           <input type="text" #txtName (keyup)="0" />
-          </h4> <my-tutorial [name] ="txtName.value"></my-tutorial>
+          <button (click) ="ChangeName()">Change Name</button>
+          <p>Agree number: {{agree}} Disagree: {{disgree}} </p>
+          </h4> <my-tutorial *ngFor ="let persion of Names" [name] ="persion" (onVote) ="parentvoted($event)"></my-tutorial>
   `  
 })
 export class AppComponent {
   public title ="App Component"
+  public agree:number =0;
+  public  disgree: number =0;
+  public Names =['Vũ','Sơn','Tuyết','Tươi']
+  parentvoted(agree:boolean)
+  {
+    if (agree)
+    {
+      this.agree++;
+    }
+    else
+    {
+      this.disgree++;
+    }
+  }
+  @ViewChild(TutorialComponent)
+  public tutorialComponent: TutorialComponent
+  ChangeName()
+  {
+    this.tutorialComponent.setName("Change Name from App Component");
+  }
+
 }
