826d138b08505ecb58aae49b6cfeb12afca5f071
Le Anh Vu <phongsonpt@yahoo.com>
2018-04-24 16:52:39 +0700

    1

--- src/app/home.component.ts
+++ src/app/home.component.ts
@@ -1,25 +1,23 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
 
 @Component
-({
-    selector: 'home-component',
-    template: `
+    ({
+        selector: 'home-component',
+        template: `
     <h1>Home Component</h1>
     <button (click) ="GotoEmployee()" >Go to Employee</button>
     `
-})
+    })
 
 
-export class HomeComponent
-{
-    constructor(private router:Router)
-    {
+export class HomeComponent {
+    constructor(private router: Router) {
 
     }
-    GotoEmployee(){
-    this.router.navigate(['employees']);
+    GotoEmployee() {
+        this.router.navigate(['employees']);
     }
 
 }
