2196e28180a7966dfe6c77aea8e7217cfe07056a
Le Anh Vu <phongsonpt@yahoo.com>
2018-04-24 17:27:56 +0700

    Lesson26

--- src/app/employee.component.ts
+++ src/app/employee.component.ts
@@ -3,6 +3,7 @@ import { EmailValidator } from '@angular/forms';
 import { EmployeeService } from './services/employee.service';
 import {Observable} from 'rxjs/Observable'
 import { error } from 'protractor';
+import {Router, ActivatedRoute} from '@angular/router';
 
 @Component ({
     selector: 'employee-list',
@@ -10,17 +11,27 @@ import { error } from 'protractor';
 })
 export class EmployeeListComponent implements OnInit
 {
+    public pages: number[];
     public employees: any[];
-    constructor(private employeeService: EmployeeService)
+    private activePage : number;
+    constructor(private employeeService: EmployeeService, private router: Router, private activatedRoute:ActivatedRoute)
     {
        
     }
     ngOnInit()
     {
+        this.pages = [1,2,3,4,5];
+        this.activatedRoute.queryParams.subscribe(params =>{
+                this.activePage = params['numberPage'] || 1;
+                alert(this.activePage);
+        })
         this.employeeService.GetList().subscribe((response:any)=> {
             this.employees =  response;
         },error =>{
             console.error("System error API: " + error)
         });
+        
+        
+
     }
 }
